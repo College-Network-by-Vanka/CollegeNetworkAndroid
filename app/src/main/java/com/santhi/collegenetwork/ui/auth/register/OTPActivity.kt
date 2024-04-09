7f9cbf583f9d23b6344ed230ed14117e949ab176
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2024-04-09 16:16:52 +0530

    first commit

---
+++ app/src/main/java/com/santhi/collegenetwork/ui/auth/register/OTPActivity.kt
@@ -0,0 +1,12 @@
+package com.santhi.collegenetwork.ui.auth.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.santhi.collegenetwork.R
+
+class OTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otpactivity)
+    }
+}
---
+++ app/src/main/java/com/santhi/collegenetwork/ui/splashScreen/WelcomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.santhi.collegenetwork.ui.splashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.santhi.collegenetwork.R
+
+class WelcomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
